911d5457b789ceb4e5fb6aa3a718c5c34fb0a058
vinod warnakulasooriya <116092558+vinuwa98@users.noreply.github.com>
2024-02-20 09:42:12 +0530

    upload

---
+++ app/src/main/java/com/s22010554/helloeei4369labs/MainActivity.java
@@ -0,0 +1,14 @@
+package com.s22010554.helloeei4369labs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
